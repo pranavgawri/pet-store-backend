cc967f697870393a17f5237153bc7213e2fda5de
Pranav Gawri <pranav.gawri@gmail.com>
2024-09-17 22:24:26 +0530

    1. Insignificant updates

--- src/main/kotlin/com/example/petstore/controller/PetController.kt
+++ src/main/kotlin/com/example/petstore/controller/PetController.kt
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.CrossOrigin
 
+// @CrossOrigin(origins = ["http://localhost:3000"])
 @RestController
 @RequestMapping("/pets")
 class PetController(private val petService: PetService) {
